a90c854a2b7c6659304dac3f7c2ffb8230e6f5e2
Jacky Lai <jacky96923@gmail.com>
2023-12-05 12:14:39 +0800

    jacky: connected to data base get data for recipe

--- db/data.sql
+++ db/data.sql
@@ -21,5 +21,21 @@ White Pepper Black Pepper Paprika Ginger Mustard Powder', 1);
 insert into recipe (user_id, title, video, cuisine_id, calories, content, diet_id) values ( 2, 'fishy fish', '/Users/kaheilai/Desktop/uncycle1.m4v', 2, 500, 'Sprinkle catfish with salt and pepper. Whisk the eggs, mustard and milk in a shallow bowl. Place flour and pretzels in separate shallow bowls. Coat fillets with flour, then dip in egg mixture and coat with pretzels. Heat 1/4 in. oil to 375° in an electric skillet. Fry fillets, a few at a time, until fish flakes easily with a fork, 3-4 minutes on each side. Drain on paper towels. Serve with lemon slices if desired.', 2);
 
 
+insert into recipe_image (recipe_id, image, is_cover) values ( 1, 'chicken.jpg', true);
+insert into recipe_image (recipe_id, image, is_cover) values ( 2, 'fish.jpg', true);
 
-insert into recipe_image (0, image) VALUES ('seafood'),('lemon');
+insert into recipe_ingredient (recipe_id, ingredient_name, amount, unit) values ( 1, 'chicken', 1, 'kg'); 
+insert into recipe_ingredient (recipe_id, ingredient_name, amount, unit) values ( 1, 'flour', 0.5, 'kg'); 
+insert into recipe_ingredient (recipe_id, ingredient_name, amount, unit) values ( 2, 'fish', 2, 'kg');
+insert into recipe_ingredient (recipe_id, ingredient_name, amount, unit) values ( 2, 'salt', 3, 'tsp');
+
+
+DELETE FROM allergies WHERE id IN (1, 2);
+
+insert into allergies (name) values ('Peanut');
+insert into allergies (name) values ('Gluten');
+
+update allergies set id = 1 where id = 3;
+update allergies set id = 2 where id = 4;
+
+insert into recipe_allergies ()
--- recipes.ts
+++ recipes.ts
@@ -1,12 +1,27 @@
 import { Router } from 'express'
-import express from "express";
 
+import { client } from './server';
 
 
 export let recipeRouter = Router()
 
 
+recipeRouter.get('/recipe/:id', async (req, res) => {
+    try {
+        let id=req.params.id
+        let result = await client.query(`select recipe.id, user_name, title as recipe_title, video, calories, content, cuisine.name as cuisine, image, ingredient_name, amount, unit, profile_pic from recipe join cuisine on recipe.cuisine_id = cuisine.id 
+        join recipe_image on recipe_image.recipe_id = recipe.id 
+        join recipe_ingredient on recipe.id= recipe_ingredient.recipe_id
+        join recipe_allergies on recipe_allergies.recipe_id  = recipe.id
+        join users on recipe.user_id = users.id
+        where recipe.id = $1`,[id])
+        let data = result.rows
+        console.log(data);
+        
+        res.json(data)
+    } catch (error) {
+        
+    }
+})
 
 
-
-recipeRouter.use(express.static('public'));

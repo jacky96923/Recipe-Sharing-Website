b9647522f99de3dc0e8852677a5efe743a184573
Ross Chan <rosschan@amusehk.com>
2023-12-05 16:17:46 +0800

    ross commit

--- public/filterResult/filterResult.js
+++ public/filterResult/filterResult.js
@@ -1,3 +1,18 @@
 // const socket = io.connect();
 
-function loadRecipes() {}
+{
+}
+
+async function loadFilterResults() {
+  let res = await fetch("/results");
+  let json = await res.json();
+  results.textContent = json.results;
+}
+loadFilterResults();
+// window.addEventListener('mouseover', loadFilterResults)
+//  window.addEventListener('visibilitychange', loadFilterResults)
+// setInterval(loadCounter, 1000)
+//   async function inc() {
+//     let res = await fetch('/counter/inc', {
+//       method: 'POST',
+//     })

f3e96f1ba1092c85a7ad2e76a7da2f47519f3b15
Jacky Lai <jacky96923@gmail.com>
2023-12-07 17:55:39 +0800

    jacky: updated submit form

--- post_recipes.ts
+++ post_recipes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import { client } from "./database"
+import formidable from 'formidable'
+import fs from 'fs'
+import { randomUUID } from 'crypto'
 
 export let post_recipeRouter = Router()
 
+//formidable
+const uploadDir = 'uploads'
+fs.mkdirSync(uploadDir, { recursive: true })
 
 post_recipeRouter.get('/attributes', async (req, res) => {
     try {
@@ -28,6 +34,50 @@ post_recipeRouter.get('/attributes', async (req, res) => {
 })
 
 post_recipeRouter.post('/submit', (req, res) => {
-    console.log(req.body)
-    res.redirect('/submit_success.html')
+    console.log('uploading')
+    let form = formidable({
+      uploadDir,
+      keepExtensions: true,
+      maxFiles: 1,
+    //  filter: part => part.mimetype?.startsWith('image/') || false,
+        filter(part) {
+        return      part.mimetype?.startsWith('image/')||false
+        },
+        filename(name, ext, part, form) {
+            let uuid = randomUUID()
+            let extName = part.mimetype?.split('/').pop()
+        return `${uuid}.${extName}`
+        },  
+    })
+    form.parse(req, async(err, fields, files) => {
+        console.log({ err, fields, files })
+//         if (err) {
+//             console.error(err)
+//             res.status(500).json({error: 'Error processing form data'})
+//         } else {
+//             let { recipe_name, Meat, Seafood, Pork, North, calories, content_input } = fields;
+
+// if(!recipe_name||!Meat){
+//     res.json({error:"missing form item"})
+//     return
+// }
+
+
+
+//             let { newfilename } = files;
+//             await client.query(
+//                 `INSERT INTO your_table_name (recipe_name, meat, seafood, pork, north, calories, content_input, image_filepath)
+//                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+//                 [recipe_name, Meat, Seafood, Pork, North, calories, content_input, files.image.newfilename]
+//               )
+
+            
+//         }
+
+
+
+
+        res.json({message:"submit success"})
+    // res.redirect('/post_recipes/submit_success.html')
+    })
 })
--- public/post_recipes/post_recipes.js
+++ public/post_recipes/post_recipes.js
@@ -1,3 +1,59 @@
 import { loadAttributes } from "../loadAttributes.js";
 
 loadAttributes();
+
+document
+  .querySelector("#form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    // Serialize the Form afterwards
+    const form = event.target;
+    const formObject = new FormData(form);
+
+    const res = await fetch("/submit", {
+      method: "POST",
+      body: formObject,
+    });
+    const result = await res.json();
+
+    console.log(result);
+  });
+
+// Add click event listener to the "Add Ingredient" button
+$("#add_ingredient_button").click(function () {
+  // Create a new set of 3 text boxes and text elements
+  var ingredientDiv = $("<div></div>");
+  var nameInput = $('<input type="text" name="name">');
+  var nameLabel = $("<div>Name: </div>");
+  var amountInput = $('<input type="text" name="amount">');
+  var amountLabel = $("<div>Amount: </div>");
+  var unitInput = $('<input type="text" name="unit">');
+  var unitLabel = $("<div>Unit: </div>");
+
+  // Create a delete button for the new set of text boxes
+  var deleteButton = $(
+    '<button class="delete_ingredient_button">Delete</button>'
+  );
+
+  // Append the new elements to the container
+  ingredientDiv.append(
+    nameLabel,
+    nameInput,
+    amountLabel,
+    amountInput,
+    unitLabel,
+    unitInput,
+    deleteButton
+  );
+  $("#additional_ingredients").append(ingredientDiv);
+});
+
+// Add click event listener to the "Delete" buttons (using event delegation)
+$("#additional_ingredients").on(
+  "click",
+  ".delete_ingredient_button",
+  function () {
+    $(this).parent().remove();
+  }
+);
